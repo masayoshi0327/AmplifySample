fcf00e971a03c51509eee8faf9c39dd91752bb75
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-03-11 17:39:34 +0900

    Initial Commit

---
+++ AmplifySample/AmplifySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmplifySampleApp.swift
+//  AmplifySample
+//
+//  Created by 澤崎正佳 on 2021/03/11.
+//
+
+import SwiftUI
+
+@main
+struct AmplifySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
